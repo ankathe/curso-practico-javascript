10731af2622eed124104d25294a903642504bb44
Katherine <angiecruzo.o@gmail.com>
2023-06-26 10:15:51 -0500

    incluyendo herramientas practicas del curso de dalto js

---
+++ Pensamiento logico/forin.js
@@ -0,0 +1,29 @@
+// // for in y for off
+// let animales = ["perro", "gato", "tiranosaurio", "paloma"];
+// let nombres = ["camilo", "john", "christina", "josefina"];
+
+// for (pets in animales){
+//     document.write(pets + "<br>");
+// }
+
+
+// historia de cofla nivel 2- curso junior JS
+// let free = false;
+// const detectorAgeTime = time => {
+//     let age = prompt('cual es tu edad');
+//     if (age >= 18){
+//         if(time >= 2 && time < 7 && (free == false)){
+//         alert('puedes ingresar a la fiesta gratis');
+//         free = true;
+//         }else{
+//             alert("podes pasar, pero tenes que pagar la entrada");
+//         }
+//     }else{
+//         alert("no puedes ingresar, eres menor de edad");
+//     }
+// }
+
+// detectorAgeTime(10);
+// detectorAgeTime(1);
+// detectorAgeTime(2);
+
---
+++ Pensamiento logico/poo.js
@@ -0,0 +1,81 @@
+class Celular{
+    constructor(color, peso, tamanio, rdc, ram){
+        this.color = color;
+        this.peso = peso;
+        this.tamanio = tamanio;
+        this.resolucionDeCamara = rdc;
+        this.memoriaRam = ram;
+        this.encendido = false;
+    }
+    botonEncendido(){
+        if(this.encendido == false){
+            alert ("celular prendido");
+            this.encendido == true;
+        } else{
+            alert("el celular ya esta apagado");
+            this.encendido == false;
+        }
+    }
+    reiniciar(){
+        if(this.encendido == true){
+            alert("reiniciando celular")
+        }else{
+            alert("el celular esta apagado");
+        }
+    }
+    tomarFotos(){
+        alert(`foto tomada en una resolucion de: ${this.resolucionDeCamara}`)
+    }
+    grabar(){
+        alert(`grabando video en: ${this.resolucionDeCamara}`)
+    }  
+    mobileInfo(){
+        return`  
+        color: <b>${this.color}</b></br>
+        peso: <b>${this.peso}</b></br>
+        tamanio: <b>${this.tamanio}</b></br>
+        resolucion de video: <b>${this.resolucionDeCamara}</b></br>
+        memoriaRam: <b>${this.memoriaRam}</b></br>
+        `;      
+        }  
+}
+
+class CelularAltaGama extends Celular{
+    constructor(color, peso, tamanio, rdc, ram, rdce){
+        super(color, peso, tamanio, rdc, ram);
+        this.resolucionDeCamaraExtra = rdce;
+    }
+    grabarVideoLento(){
+        alert("estas grabando en camara lenta");
+    }
+    reconocimientoFacial(){
+        alert("vamos a iniciar un reconocimiento facial")
+    }
+    infoAltaGama(){
+        return this.mobileInfo() + `resolucion de camara trasera: <b>${this.resolucionDeCamaraExtra}</b>`;
+    }
+}
+
+// celular1 = new Celular("rojo", "150g", "5 pulgadas", "HD", "1GB");
+// celular2 = new Celular("negro", "155g", "5.4 pulgadas", "full HD", "2GB");
+// celular3 = new Celular("blanco", "15046g", "5.9 pulgadas", "full HD", "2GB");
+
+// celular1.botonEncendido();
+// celular1.tomarFotos();
+// celular1.grabar();
+// celular1.reiniciar();
+// celular1.botonEncendido();
+
+celular1 = new CelularAltaGama("red", "150g", "5 ", "4K", "3GB", "full HD");
+celular2 = new CelularAltaGama("black", "155g", "5.4 ", "4K", "4GB", "HD");
+
+
+document.write(`
+${celular1.infoAltaGama()}</br></br>
+${celular2.infoAltaGama()}</br>
+`);
+
+
+
+
+
