d54f1208830af50b6c93c939d65a4cb06df281f1
Katherine <angiecruzo.o@gmail.com>
2023-07-05 16:38:56 -0500

    solucuin problema cofla capitulo 5

---
+++ Pensamiento logico/console.js
@@ -0,0 +1,50 @@
+// let average = prompt("cual es tu promedio?");
+// let practiceWorks = Prompt("que porcentaje de trabajos practicos entregaste?");
+// if (average >= 7 & practiceWorks >= 75){
+//     console.log("%cAprobaste", "color: #008000");
+// }else{
+//     console.log("%cNo aprobaste", "color: #FF0000");
+// }
+
+const materias = {
+	fisica: [90,6,3,"fisica"],
+	matematica: [84,8,2,"matematica"],
+	logica: [92,8,4,"logica"],
+	quimica: [96,8,4,"quimica"],
+	calculo: [91,6,3,"calculo"],
+	programacion: [79,7,4,"programacion"],
+	biologia: [75,9,2,"biologia"],
+	bbdd: [98,9,1,"bbdd"],
+	algebra: [100,10,4,"algebra"]
+}
+
+const aprobo = ()=>{
+	for (materia in materias){
+
+		let asistencias = materias[materia][0];
+		let promedio = materias[materia][1];
+		let trabajos = materias[materia][2]
+
+		console.log(materias[materia][3]);
+
+		if (asistencias >= 90) {
+			console.log("%c   Asistencias en orden","color:green");
+		} else {
+			console.log("%c   Falta de Asistencias","color:red");
+		}
+
+		if (promedio >= 7) {
+			console.log("%c   Promedio en orden","color:green");
+		} else {
+			console.log("%c   Promedio desaprobado","color:red");
+		}
+
+		if (trabajos >= 3) {
+			console.log("%c   Trabajos prácticos en Orden","color:green");
+		} else {
+			console.log("%c   Faltan trabajos prácticos","color:red");
+		}
+	}
+}
+
+aprobo()
